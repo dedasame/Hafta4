395921f668cbf779066427124f18e5bdb4be777b
eceka <eceka@lenovo>
2022-10-27 10:31:17 +0300

    second

--- Hafta4/src/InitArrays.java
+++ Hafta4/src/InitArrays.java
@@ -1,22 +1,28 @@
-
 public class InitArrays {
 
 	public static void main(String[] args) {
-		
 		// int[] array = new int[10];
-		
 		int[] array = {32,27,64,18,95,14,90,70,60,37};
-		
 		
 		System.out.printf("%s%8s%n","Index","Value");
 		
 		for(int counter = 0;counter<array.length;counter++) {
-			
-			System.out.printf("%5d%8d%n",counter,array[counter]);
+			System.out.printf("%5d%8d%n ",counter,array[counter]);
+			}
 		
-			
+		System.out.println("Ters cevrilmis hali");
+		
+		for(int i=0;i<array.length/2;i++) {
+			int tmp;
+			tmp=array[i];
+			array[i]=array[(array.length-1)-i];
+			array[(array.length-1)-i]=tmp;		
+		}	
+		
+		for(int j=0;j<array.length;j++) {
+			System.out.printf("%5d%8d%n",j,array[j]);	
 		}
 		
+		
 	}
-
 }
